99f187baa8eba2e063e73caafbc94accaf175d52
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-07-08 10:46:02 -0300

    improve gameController script

--- scripts.js
+++ scripts.js
@@ -20,9 +20,30 @@ function playerGenerator(name, mark) {
       mark:  mark
     }
 }
-
 const player1 = playerGenerator("player1", "X");
 const player2 = playerGenerator("player2", "O");
+
+function gameController() {
+  let currentPlayer = player1;
+  let switchTurn = () => {
+    if(currentPlayer === player1) {
+        currentPlayer = player2;
+      }
+      else {
+        currentPlayer = player1;
+      }
+  }
+  return {
+    switchTurn: switchTurn,
+    getCurrentPlayer: () => currentPlayer,
+    playRound: (index) => {
+      board.placeMark(index, currentPlayer.mark);
+      switchTurn();
+    }
+  }
+}
+
+const game = gameController();
 
 
 
