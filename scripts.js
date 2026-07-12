2ec719b54def92c22fe3bfd996ba56ef369e65ee
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-07-12 16:35:30 -0300

    change the import method of svg icons to innerHTML

--- scripts.js
+++ scripts.js
@@ -93,21 +93,11 @@ function displayController() {
         // Replace X/O text with SVG icon
         if(square.textContent === "X") {
           square.textContent = "";
-
-          const xIcon = document.createElement("img");
-            xIcon.src = "icons/x.svg";
-            xIcon.alt = "x icon";
-            xIcon.classList.add("x-icon");
-          square.appendChild(xIcon);
+          square.innerHTML = `<svg class="x-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
         }
         else if(square.textContent === "O") {
           square.textContent = "";
-
-          const circleIcon = document.createElement("img");
-            circleIcon.src = "icons/circle.svg";
-            circleIcon.alt = "circle icon";
-            circleIcon.classList.add("circle-icon");
-          square.appendChild(circleIcon);
+          square.innerHTML = `<svg class="circle-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`;
         }
         grid.appendChild(square);
       }
