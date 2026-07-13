5550a77f6efa202cf34e03ec571f1885baeeb887
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-07-12 21:07:17 -0300

    add square event listener

--- scripts.js
+++ scripts.js
@@ -86,21 +86,31 @@ function displayController() {
 
   return {
     createGrid: () => {
-      for(let i = 0; i < board.getBoard().length; i++) {
-        let square = document.createElement("div");
-          square.classList.add("square");
-          square.textContent = board.getBoard()[i];
-        
-        // Replace X/O text with SVG icon
-        if(square.textContent === "X") {
+       // Replace X/O text with SVG icon
+      let updateSquare = (square, index) => {
+        square.textContent = board.getBoard()[index];
+
+        if (square.textContent === "X") {
           square.textContent = "";
           square.innerHTML = `<svg class="x-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>`;
         }
-        else if(square.textContent === "O") {
+        else if (square.textContent === "O") {
           square.textContent = "";
           square.innerHTML = `<svg class="circle-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`;
         }
-        grid.appendChild(square);
+      }
+  
+      for(let i = 0; i < board.getBoard().length; i++) {
+        let square = document.createElement("div");
+          square.classList.add("square");
+          updateSquare(square, i);
+
+        square.addEventListener("click", () => {
+          game.playRound(i);
+          updateSquare(square, i);
+        });
+
+        grid.appendChild(square);        
       }
     }
   }
@@ -122,19 +132,19 @@ closeButton.addEventListener("click", () => {
   form.reset();
 });
 
+
 // TESTS
-game.playRound(0);
-game.playRound(1);
-game.playRound(2);
-game.playRound(3);
-game.playRound(4);
-game.playRound(5);
-game.playRound(6);
-game.playRound(7);
-game.playRound(8);
+// game.playRound(0);
+// game.playRound(1);
+// game.playRound(2);
+// game.playRound(3);
+// game.playRound(4);
+// game.playRound(5);
+// game.playRound(6);
+// game.playRound(7);
+// game.playRound(8);
 display.createGrid();
 
 // Refactor gameController() logic
-// Build Dialog tag
 
 
